e9ed1fdcb51facee4f63a9d11497f18c0cfecbc3
Oleg Radchenko <oleg@deepinspire.com>
2017-02-21 00:00:52 +0200

    fix

--- controllers/BooksController.php
+++ controllers/BooksController.php
@@ -134,10 +134,7 @@ class BooksController extends Controller
         $model = new Books(); 
 		
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-			Yii::$app->mailer->compose([
-					'html' => 'contact-html',
-					'text' => 'contact-text',
-				])
+			Yii::$app->mailer->compose()
 				->setTo(Yii::$app->getUser()->getIdentity()->email)
 				->setFrom(['noreply@knigu.org' => Yii::t('app', 'send_sticker_from_title')])
 				->setSubject(Yii::t('app', 'send_sticker_subject "{title}"', ['title' => $model->title]))
@@ -159,10 +156,7 @@ class BooksController extends Controller
     {
 		$model = $this->findModel($id);
 		
-		Yii::$app->mailer->compose([
-				'html' => 'contact-html',
-				'text' => 'contact-text',
-			])
+		Yii::$app->mailer->compose()
 			->setTo(Yii::$app->getUser()->getIdentity()->email)
 			->setFrom(['noreply@knigu.org' => Yii::t('app', 'send_sticker_from_title')])
 			->setSubject(Yii::t('app', 'send_sticker_subject "{title}"', ['title' => $model->title]))
